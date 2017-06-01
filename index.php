70ac921f58269a09d07adb2c57402c7500151e14
sarvari1996 <sarvari1996@gmail.com>
2017-06-01 23:51:15 +0200

    sadas

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
 <title>HTML Tutorial</title>
 <body>
 
+<table>
+	<tr>
+		<td>
+			<h1> hinnye
+			</h1>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h2> hunnye
+			</h2>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h3> hennye
+			</h3>
+		</td>
+	</tr>
+</table>
+
 
 </body>
 </html>
